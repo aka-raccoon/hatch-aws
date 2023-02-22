b0efff07afa705e2c6f4771c13c38865c9e68725
aka-raccoon <aka-raccoon@pm.me>
2023-02-22 13:36:03 +0100

    refactor: optimize AwsLambda model

--- src/hatch_aws/aws.py
+++ src/hatch_aws/aws.py
@@ -1,53 +1,19 @@
-import logging
-from contextlib import contextmanager
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from os import sep
 from pathlib import Path
 from subprocess import run  # nosec
-from typing import Dict, Generator, List, Optional, Union
+from typing import Dict, List, Optional
 
 import yaml
-
-from hatch_aws.exceptions import ParameterIsMissing, UnsupportedTypeForParameter
 
 
 @dataclass
 class AwsLambda:
-    default_code_uri: Union[str, Dict, None]
-    default_handler: Union[str, Dict, None]
-    resource: Dict
     name: str
-    code_uri: Path = field(init=False)
-    handler: Path = field(init=False)
-    module: Path = field(init=False)
-
-    def __post_init__(self):
-        self.code_uri = self._set_code_uri()
-        self.handler = self._set_handler()
-        self.module = self.handler.parent.parent
-
-    def _get_property_value(self, default_val: Union[str, Dict, None], param: str) -> str:
-        try:
-            value = self.resource["Properties"][param]
-        except KeyError as error:
-            if not default_val:
-                raise ParameterIsMissing(lambda_name=self.name, parameter=param) from error
-            value = default_val
-
-        if not isinstance(value, str):
-            raise UnsupportedTypeForParameter(lambda_name=self.name, parameter=param)
-        return value
-
-    def _set_handler(self) -> Path:
-        value = self._get_property_value(default_val=self.default_handler, param="Handler")
-        value = value.replace(".", "/")
-        return Path(value)
-
-    def _set_code_uri(self) -> Path:
-        value = self._get_property_value(default_val=self.default_code_uri, param="CodeUri")
-        return Path(value)
+    path: Path
 
 
-class Sam:
+class Sam:  # pylint: disable=too-few-public-methods
     def __init__(self, template: Path):
         self.template_path = template
         self.template = self._parse_sam_template()
@@ -55,23 +21,22 @@ class Sam:
 
     def _get_aws_lambdas(self) -> List[AwsLambda]:
         resources = self.template["Resources"]
+        lambdas = {
+            resource: {
+                **self.template.get("Globals", {}).get("Function", {}),
+                **resources[resource]["Properties"],
+            }
+            for resource, param in resources.items()
+            if param["Type"] == "AWS::Serverless::Function"
+        }
         return [
             AwsLambda(
                 name=resource,
-                resource=resources[resource],
-                default_code_uri=self._get_global_code_uri(),
-                default_handler=self._get_global_handler(),
+                path=Path(param["Handler"].replace(".", sep)).parent.parent,
             )
-            for resource, param in resources.items()
-            if param["Type"] == "AWS::Serverless::Function"
+            for resource, param in lambdas.items()
+            if param.get("Runtime", "").lower().startswith("python")
         ]
-
-    @staticmethod
-    @contextmanager
-    def disable_sam_logging() -> Generator[None, None, None]:
-        logging.disable(logging.CRITICAL)
-        yield
-        logging.disable(logging.NOTSET)
 
     def _parse_sam_template(self) -> Dict:
         yaml.SafeLoader.add_multi_constructor("!", lambda *args: None)
@@ -86,15 +51,3 @@ class Sam:
         return run(
             ["sam", "build"] + params, text=True, encoding="utf-8", capture_output=True, check=False
         )
-
-    def _get_global_code_uri(self) -> Union[Dict, str, None]:
-        try:
-            return self.template["Globals"]["Function"]["CodeUri"]
-        except KeyError:
-            return None
-
-    def _get_global_handler(self) -> Union[Dict, str, None]:
-        try:
-            return self.template["Globals"]["Function"]["Handler"]
-        except KeyError:
-            return None
